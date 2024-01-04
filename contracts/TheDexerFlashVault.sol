1da86f134fa3ee1b00506d97f3a9e97ab63250fd
wojakpepechad <tokenalert7@gmail.com>
2024-01-04 22:38:27 +0100

    Update TheDexerFlashVault.sol

--- contracts/TheDexerFlashVault.sol
+++ contracts/TheDexerFlashVault.sol
@@ -2,127 +2,128 @@
 pragma solidity ^0.8.20;
 
 interface IERC20 {
-  function transfer(address to, uint256 amount) external returns (bool);
+    function transfer(address to, uint256 amount) external returns (bool);
 
-  function transferFrom(
-    address from,
-    address to,
-    uint256 amount
-  ) external returns (bool);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
 
-  function balanceOf(address account) external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    
+    function approve(address spender, uint256 value) external returns (bool);
 
-  function approve(address spender, uint256 value) external returns (bool);
 }
 
 interface IFlashLoanReceiver {
-  function executeOperation(
-    address[] calldata tokens,
-    uint256[] calldata amounts,
-    uint256[] calldata premiums
-  ) external;
+    function executeOperation(uint256 amount, uint256 premium) external;
 }
 
 contract TheDexerFlashVault {
-  uint256 public fee;
-  mapping(address => bool) public excluded;
+    IERC20 public token;
+    uint public fee;
+    mapping(address => uint256) public deposits;
+    mapping(address => bool) public excluded;
 
-  address public owner;
+    address public owner;
 
-  constructor() {
-    excluded[msg.sender] = true;
-    owner = msg.sender;
-    fee = 1;
-  }
-
-  modifier onlyOwner() {
-    require(msg.sender == owner, 'Not the owner');
-    _;
-  }
-
-  function exclude(address user) external onlyOwner {
-    excluded[user] = true;
-  }
-
-  function setFees(uint256 _fee) external onlyOwner {
-    fee = _fee;
-  }
-
-  function deposit(address tokenAddress, uint256 amount) external {
-    IERC20 token = IERC20(tokenAddress);
-    require(token.transferFrom(msg.sender, address(this), amount), 'Transfer failed');
-  }
-
-  function withdraw(address tokenAddress, uint256 amount) external onlyOwner {
-    IERC20 token = IERC20(tokenAddress);
-    require(token.transfer(msg.sender, amount), 'Transfer failed');
-  }
-
-  function flashLoan(address[] calldata tokens, uint256[] calldata amounts) external {
-    require(tokens.length == amounts.length, 'Mismatched tokens and amounts');
-
-    uint256[] memory premiums = new uint256[](tokens.length);
-    uint256[] memory balancesBefore = new uint256[](tokens.length); // Store initial balances
-
-    // Transfer out tokens and record balances before the loan
-    for (uint256 i = 0; i < tokens.length; i++) {
-      address tokenAddress = tokens[i];
-      uint256 amount = amounts[i];
-      IERC20 token = IERC20(tokenAddress);
-
-      balancesBefore[i] = token.balanceOf(address(this));
-      require(balancesBefore[i] >= amount, 'Insufficient balance');
-
-      premiums[i] = !excluded[msg.sender] ? (amount * fee) / 1000 : 0;
-
-      require(token.transfer(msg.sender, amount), 'Failed to transfer tokens');
+    constructor(address _tokenAddress) {
+        token = IERC20(_tokenAddress);
+        excluded[msg.sender] = true;
+        owner = msg.sender;
+        fee = 1;
     }
 
-    // Execute operation with the loaned tokens
-    IFlashLoanReceiver(msg.sender).executeOperation(tokens, amounts, premiums);
-
-    // Check if loans are repaid
-    for (uint256 i = 0; i < tokens.length; i++) {
-      address tokenAddress = tokens[i];
-      IERC20 token = IERC20(tokenAddress);
-
-      // Use the stored balance before to check repayment
-      uint256 requiredBalanceAfter = balancesBefore[i] + premiums[i];
-      require(token.balanceOf(address(this)) >= requiredBalanceAfter, 'Loan not repaid');
-    }
-  }
-
-  function flashLoanWallet(address[] calldata tokens, uint256[] calldata amounts) external {
-    require(tokens.length == amounts.length, 'Mismatched tokens and amounts');
-
-    uint256[] memory premiums = new uint256[](tokens.length);
-    uint256[] memory balancesBefore = new uint256[](tokens.length);
-
-    // Transfer tokens from the owner to the borrower and record the initial balances
-    for (uint256 i = 0; i < tokens.length; i++) {
-      address tokenAddress = tokens[i];
-      uint256 amount = amounts[i];
-      IERC20 token = IERC20(tokenAddress);
-
-      balancesBefore[i] = token.balanceOf(address(this));
-      premiums[i] = !excluded[msg.sender] ? (amount * fee) / 1000 : 0;
-
-      require(token.transferFrom(owner, address(this), amount), 'Transfer failed');
-      require(token.transfer(msg.sender, amount), 'Failed to transfer tokens');
+    function exclude(address user) external {
+        require(msg.sender == owner);
+        excluded[user] = true;
     }
 
-    // Execute the borrower's operations with the loaned tokens
-    IFlashLoanReceiver(msg.sender).executeOperation(tokens, amounts, premiums);
-
-    // Check if the loans are repaid and transfer premiums to the owner
-    for (uint256 i = 0; i < tokens.length; i++) {
-      IERC20 token = IERC20(tokens[i]);
-      uint256 requiredBalanceAfter = balancesBefore[i] + premiums[i];
-      require(token.balanceOf(address(this)) >= requiredBalanceAfter, 'Loan not repaid');
-
-      if (premiums[i] > 0) {
-        require(token.transfer(owner, requiredBalanceAfter), 'Failed to transfer premium');
-      }
+    function setFees(uint _fee) external {
+        require(msg.sender == owner);
+        fee = _fee;
     }
-  }
+
+
+    function deposit(uint256 amount) external {
+        require(
+            token.transferFrom(msg.sender, address(this), amount),
+            "Transfer failed"
+        );
+        deposits[msg.sender] += amount;
+    }
+
+    function withdraw(uint256 amount) external {
+        require(deposits[msg.sender] >= amount, "Insufficient balance");
+        deposits[msg.sender] -= amount;
+        require(token.transfer(msg.sender, amount), "Transfer failed");
+    }
+
+    function flashLoan(uint256 amount) external {
+        uint256 balanceBefore = token.balanceOf(address(this));
+        require(balanceBefore >= amount, "Insufficient balance");
+        uint256 premium = 0;
+
+        if (!excluded[msg.sender]) {
+            premium = amount*fee/1000;
+        }
+      // Transfer tokens to the receiver
+        require(
+            token.transfer(msg.sender, amount),
+            "Failed to transfer tokens to receiver"
+        );
+
+        IFlashLoanReceiver(msg.sender).executeOperation(amount, premium);
+
+        // Check if the loan is repaid
+        require(
+            token.balanceOf(address(this)) >= balanceBefore + premium,
+            "Loan not repaid"
+        );
+    }
+
+        function flashLoanWallet(uint256 amount) external {
+        uint256 balanceBefore = token.balanceOf(owner);
+        require(balanceBefore >= amount, "Insufficient balance");
+        uint256 premium = 0;
+        
+        require(
+            token.transferFrom(owner, address(this), amount),
+            "Transfer failed"
+        );
+
+        if (!excluded[msg.sender]) {
+            premium = amount*fee/1000;
+        }
+
+        // Transfer tokens to the receiver
+        require(
+            token.transfer(msg.sender, amount),
+            "Failed to transfer tokens to receiver"
+        );
+
+        IFlashLoanReceiver(msg.sender).executeOperation(amount, premium);
+
+
+
+                // Transfer tokens to the receiver
+        require(
+            token.transfer(owner, amount),
+            "Failed to transfer tokens to receiver"
+        );
+
+                // Check if the loan is repaid
+        require(
+            token.balanceOf(owner) >= balanceBefore,
+            "Loan not repaid"
+        );
+
+        if (premium > 0) {
+                    require(
+            token.transfer(owner, token.balanceOf(address(this))),
+            "Failed to transfer premium to receiver"
+        );
+        }
+    }
 }
